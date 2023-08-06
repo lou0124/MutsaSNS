93767a7544601bd448fa523e9b998f9e34a88a3d
OH CHANGMIN <lou0124@naver.com>
2023-08-06 11:48:02 +0900

    refactor: 등록 일 기준 대표 이미지 조회

--- src/main/java/ohchangmin/sns/repository/ArticleImageRepository.java
+++ src/main/java/ohchangmin/sns/repository/ArticleImageRepository.java
@@ -13,8 +13,8 @@ import java.util.Optional;
 @Repository
 public interface ArticleImageRepository extends JpaRepository<ArticleImage, Long> {
 
-    @Query("select ai from ArticleImage ai join fetch ai.article where ai.article in :articles and ai.id = " +
-        "(select min(ai2.id) from ArticleImage ai2 where ai2.article = ai.article)")
+    @Query("select ai from ArticleImage ai join fetch ai.article where ai.article in :articles and ai.createdDate = " +
+        "(select min(ai2.createdDate) from ArticleImage ai2 where ai2.article = ai.article)")
     List<ArticleImage> findInArticle(@Param("articles") List<Article> articles);
 
     @Query("select ai from ArticleImage ai join fetch ai.article a join fetch a.user u where ai.id = :articleImageId")
